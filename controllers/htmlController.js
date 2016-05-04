33f27f12d8e40dd6a4a7866c56c990159bc8cc4f
HugoMatilla <hugomatilla@gmail.com>
2016-05-05 00:08:43 +0200

    Add todos page with list

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -7,8 +7,16 @@ module.exports = function(app) {
     app.use(bodyParser.urlencoded({ extended: true }));
     
     app.get('/', function(req, res) {
-        
        res.render('index');
+    });
+    
+    app.get('/todos/:username', function(req, res) {
+         Todos.find({ username: req.params.username }, function(err, todos) {
+            if (err) throw err;
+            
+            console.log(todos)
+            res.render('todos', { ID: req.params.id, TODOS: todos });    
+        });
         
     });
 }
