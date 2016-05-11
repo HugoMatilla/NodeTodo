7f6770c3671d897e6f8cda4f691073e6660e4e9e
HugoMatilla <hugomatilla@gmail.com>
2016-05-11 10:03:29 +0200

    Add local database

--- config/index.js
+++ config/index.js
@@ -3,7 +3,8 @@ var configValues = require('./config');
 module.exports = {
     
     getDbConnectionString: function() {
-        return `mongodb://${configValues.uname}:${configValues.pwd}@ds013212.mlab.com:13212/nodetodo`;
+        //return `mongodb://${configValues.uname}:${configValues.pwd}@ds013212.mlab.com:13212/nodetodo`;
+        return 'mongodb://localhost/test';
     }
     
 }
